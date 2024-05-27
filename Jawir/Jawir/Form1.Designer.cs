320f6dd614f921fd5e5c7cb61cab11e0c2309089
darren <darrenarmani666@gmail.com>
2024-05-27 19:18:39 +0700

    tch

---
+++ Jawir/Jawir/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace Jawir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.redbar = new System.Windows.Forms.TrackBar();
+            this.greenbar = new System.Windows.Forms.TrackBar();
+            this.bluebar = new System.Windows.Forms.TrackBar();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.redvalue = new System.Windows.Forms.Label();
+            this.greenvalue = new System.Windows.Forms.Label();
+            this.bluevalue = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redbar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenbar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bluebar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(901, 568);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(924, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "None";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(924, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(225, 44);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Sepia";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(924, 112);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(225, 44);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Artistic";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(924, 162);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(225, 44);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Gray";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(924, 212);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(225, 44);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Fog";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(924, 262);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(225, 44);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Flash";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(924, 312);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(225, 44);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Frozen";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(924, 362);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(225, 44);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Suji";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(924, 412);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(225, 44);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Dramatic";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(924, 462);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(225, 44);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Kakao";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 586);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Red";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 632);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Green";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 685);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Blue";
+            // 
+            // redbar
+            // 
+            this.redbar.Location = new System.Drawing.Point(99, 586);
+            this.redbar.Maximum = 100;
+            this.redbar.Name = "redbar";
+            this.redbar.Size = new System.Drawing.Size(819, 56);
+            this.redbar.TabIndex = 16;
+            this.redbar.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            this.redbar.ValueChanged += new System.EventHandler(this.trackBar2_ValueChanged);
+            // 
+            // greenbar
+            // 
+            this.greenbar.Location = new System.Drawing.Point(99, 632);
+            this.greenbar.Maximum = 100;
+            this.greenbar.Name = "greenbar";
+            this.greenbar.Size = new System.Drawing.Size(819, 56);
+            this.greenbar.TabIndex = 17;
+            this.greenbar.Scroll += new System.EventHandler(this.trackBar2_ValueChanged);
+            this.greenbar.ValueChanged += new System.EventHandler(this.trackBar1_value);
+            // 
+            // bluebar
+            // 
+            this.bluebar.Location = new System.Drawing.Point(99, 685);
+            this.bluebar.Maximum = 100;
+            this.bluebar.Name = "bluebar";
+            this.bluebar.Size = new System.Drawing.Size(819, 56);
+            this.bluebar.TabIndex = 18;
+            this.bluebar.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(924, 586);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(225, 44);
+            this.button11.TabIndex = 19;
+            this.button11.Text = "Open Image";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(924, 636);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(225, 44);
+            this.button12.TabIndex = 20;
+            this.button12.Text = "Save Image";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // redvalue
+            // 
+            this.redvalue.AutoSize = true;
+            this.redvalue.Location = new System.Drawing.Point(69, 586);
+            this.redvalue.Name = "redvalue";
+            this.redvalue.Size = new System.Drawing.Size(24, 16);
+            this.redvalue.TabIndex = 21;
+            this.redvalue.Text = "0,0";
+            // 
+            // greenvalue
+            // 
+            this.greenvalue.AutoSize = true;
+            this.greenvalue.Location = new System.Drawing.Point(69, 685);
+            this.greenvalue.Name = "greenvalue";
+            this.greenvalue.Size = new System.Drawing.Size(24, 16);
+            this.greenvalue.TabIndex = 22;
+            this.greenvalue.Text = "0,0";
+            // 
+            // bluevalue
+            // 
+            this.bluevalue.AutoSize = true;
+            this.bluevalue.Location = new System.Drawing.Point(69, 632);
+            this.bluevalue.Name = "bluevalue";
+            this.bluevalue.Size = new System.Drawing.Size(24, 16);
+            this.bluevalue.TabIndex = 23;
+            this.bluevalue.Text = "0,0";
+            this.bluevalue.Click += new System.EventHandler(this.bluevalue_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1162, 741);
+            this.Controls.Add(this.bluevalue);
+            this.Controls.Add(this.greenvalue);
+            this.Controls.Add(this.redvalue);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.bluebar);
+            this.Controls.Add(this.greenbar);
+            this.Controls.Add(this.redbar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redbar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenbar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bluebar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar redbar;
+        private System.Windows.Forms.TrackBar greenbar;
+        private System.Windows.Forms.TrackBar bluebar;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label redvalue;
+        private System.Windows.Forms.Label greenvalue;
+        private System.Windows.Forms.Label bluevalue;
+    }
+}
+
